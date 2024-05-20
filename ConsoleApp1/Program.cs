af3cb9b521e40b13a625ff692d6746e3a2d00549
Aneel Grewal <aneel.grewal@hotmail.com>
2024-05-20 11:58:05 +0200

    ReadLine() vs Read() vs ReadKey()

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.Write("Enter your name:");
+        var name = Console.ReadLine(); //press enter to finish
+        Console.WriteLine($"Name: {name}");
+        
+        Console.Write("Enter any character to know its ASCII value:");
+        var ch = Console.Read(); //press enter to finish
+        Console.WriteLine($"ASCII value: {ch}");
+        Console.ReadKey();
+
+        Console.Write("Press any key on keyboard:");
+        var key = Console.ReadKey(); // it does not require to press the enter key 
+        Console.WriteLine();
+        Console.WriteLine($"You pressed: {key.Key}");
+
+    }
+}
